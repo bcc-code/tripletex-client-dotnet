cda4e0085a2d65facd24de2fcbc321495a879744
Reng van Oord <reng.vanoord@bcc.no>
2022-12-06 22:09:09 +0100

    Take 2

--- BccCode.Tripletex.Client/TripletexClientInstaller.cs
+++ BccCode.Tripletex.Client/TripletexClientInstaller.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BccCode.Tripletex.Client;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
